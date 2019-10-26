4655c15d37f5084c0a6a2a37c999220d21275e1e
CrushedLive <jordan@bugbird.co>
2019-10-26 21:00:00 +0100

    done

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,124 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+    <b-container id="root" class="bg-dark" fluid @click="clearState">
+        <b-row align-h="center" align-v="center" class="vh-100">
+            <b-col cols="6">
+                <h1 class="text-light text-center mb-4">CIDR Checker</h1>
+                <b-list-group v-if="ips.length" class="mb-4">
+                    <b-list-group-item v-for="(ip, i) in ips" :variant="matches(i)"
+                                       class="d-flex justify-content-between align-items-center">
+                        {{ ip.base }}/{{ ip.bitmask }}
+                        <b-button variant="link" class="close" aria-label="Close" @click="remove(i)">&times;</b-button>
+                    </b-list-group-item>
+                </b-list-group>
+                <b-form @submit.prevent="doSearch" autocomplete="off">
+                    <b-form-group class="mb-4">
+                        <b-form-input type="text" v-model="newIp" class="text-light" :class="{[`bg-${bg}`]: true}"
+                                      :state="state" @input="clearState" @blur="clearState"
+                                      @keydown.tab.prevent="toggleTest"
+                                      @keydown.shift.delete.prevent="reset"></b-form-input>
+                    </b-form-group>
+                    <b-form-group v-if="!testing" class="text-right">
+                        <b-button-group>
+                            <b-button variant="danger" @click="reset">
+                                Reset
+                            </b-button>
+                            <b-button variant="primary" @click="toggleTest">
+                                Test IPs
+                            </b-button>
+                            <b-button variant="success" type="submit">
+                                Add Address
+                            </b-button>
+                        </b-button-group>
+                    </b-form-group>
+                    <b-form-group v-else class="text-right">
+                        <b-button-group>
+                            <b-button variant="danger" @click="reset">
+                                Reset
+                            </b-button>
+                            <b-button variant="primary" @click="toggleTest">
+                                Add Addresses
+                            </b-button>
+                            <b-button variant="success" type="submit">
+                                Test IP
+                            </b-button>
+                        </b-button-group>
+                    </b-form-group>
+                </b-form>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+    import {Netmask} from 'netmask';
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    export default {
+        data() {
+            return {
+                testing: false,
+                newIp: null,
+                state: null,
+                matched: null,
+                ips: [],
+            }
+        },
+        methods: {
+            remove(i) {
+                this.ips[i] = null;
+                this.ips = this.ips.filter(Boolean);
+            },
+            reset() {
+                this.ips = [];
+            },
+            clearState() {
+                this.matched = null;
+                this.state = null;
+            },
+            toggleTest() {
+                this.state = null;
+                this.testing = !this.testing;
+            },
+            doSearch(...a) {
+                return this.testing ? this.test(...a) : this.addIp(...a);
+            },
+            addIp() {
+                try {
+                    this.ips.push(new Netmask(this.newIp));
+                    this.newIp = null;
+                    this.state = true;
+                } catch (e) {
+                    this.state = false;
+                }
+            },
+            test() {
+                try {
+                    for(let index in this.ips) {
+                        if(this.ips[index].contains(this.newIp)){
+                            this.state = true;
+                            this.matched = parseInt(index);
+                            break;
+                        }
+                    }
+                    if(!this.state) {
+                        this.state = false;
+                    }
+                    this.newIp = null;
+                } catch (e) {
+                    this.state = false;
+                }
+            },
+            matches(i) {
+                return this.matched === i ? 'success' : 'dark'
+            }
+        },
+        computed: {
+            bg() {
+                if (this.testing && this.state !== null) {
+                    return this.state ? 'success' : 'danger'
+                } else {
+                    return 'dark';
+                }
+            }
+        }
     }
-  }
-}
-</style>
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-
-Vue.config.productionTip = false
+import router from './plugins/router'
+import store from './plugins/store'
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
